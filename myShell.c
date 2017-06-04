7c3e000168a553d1452bc9f33f38606efbffa063
Danis Fermi <danisfermijohn@gmail.com>
2017-06-04 08:17:36 +0530

    Added Argument Number Check

--- myShell.c
+++ myShell.c
@@ -1,10 +1,29 @@
 #include<stdio.h>
+#include<stdlib.h>
+// Read and Parse from Config File
+void readConfig()
+{
 
-int main(int argc, char **argv)
+}
+
+// When myShell is called Interactively
+
+
+// When myShell is called with a Script as Argument
+
+
+int main(int argc, char *argv)
 {
 	// Read from myShell Configuration Files
-
+	readConfig();
 	// Parsing commands Interactive mode or Script Mode
+	if (argc==0)
+		myShellInteract();
+	else if (argc==1)
+		myShellScript(argv[1]);
+	else
+		printf("\nInvalid Number of Arguments");
 
 	// Exit the Shell
+	return EXIT_SUCCESS;
 }
